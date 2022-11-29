6f0621f494e4400b6d6d3db0a1c83db0bedc4149
Zlakowski <zlakos@o2.pl>
2022-11-29 22:45:20 +0100

    check palindrome

---
+++ check_palindrome.py
@@ -0,0 +1,19 @@
+def check_palindrome(text):
+    """ Check if given text is palindrome
+    :param str text: text
+    :rtype: bool
+    :return: True if given text is palindrome and False if not
+    """
+    text = text.lower().replace(" ", "")
+    text_reverse = text[::-1]
+    if text_reverse == text:
+        return True
+    else:
+        return False
+
+
+if __name__ == "__main__":
+    text = "ZakoPane na Pokaz"
+    print(check_palindrome(text))
+    text = "Palidrom to nie jest"
+    print(check_palindrome(text))
